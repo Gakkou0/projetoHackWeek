aa6bff4e5ecb9342dd9ad01ff5c301d911c6cb58
luisgbr1el <harrycorpmail@gmail.com>
2023-05-20 23:43:37 -0300

    UserForm default

--- web/src/components/User/UserForm/UserForm.js
+++ web/src/components/User/UserForm/UserForm.js
@@ -1,12 +1,13 @@
+/* eslint-disable prettier/prettier */
 import {
+  Form,
+  FormError,
+  FieldError,
+  Label,
   TextField,
-  InputLabel,
-  Select,
-  MenuItem,
-  FormControl,
-} from '@mui/material'
-
-import { Form, FormError, FieldError, Submit } from '@redwoodjs/forms'
+  NumberField,
+  Submit,
+} from '@redwoodjs/forms'
 
 const UserForm = (props) => {
   const onSubmit = (data) => {
@@ -23,34 +24,49 @@ const UserForm = (props) => {
           listClassName="rw-form-error-list"
         />
 
+        <Label
+          name="name"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          Name
+        </Label>
+
         <TextField
           name="name"
           defaultValue={props.user?.name}
-          label="Nome"
+          className="rw-input"
+          errorClassName="rw-input rw-input-error"
           validation={{ required: true }}
-          style={{ width: '300px' }}
         />
 
         <FieldError name="name" className="rw-field-error" />
 
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">Tipo da conta</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            name="userType"
-            value={props.user?.userType}
-            label="Tipo da conta"
-            style={{ width: '300px' }}
-            // onChange={handleChange}
-          >
-            <MenuItem value={0}>Estudante</MenuItem>
-            <MenuItem value={1}>Orientador</MenuItem>
-            <MenuItem value={2}>Co-orientador</MenuItem>
-          </Select>
-        </FormControl>
+        <Label
+          name="userType"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          User type
+        </Label>
+
+        <NumberField
+          name="userType"
+          defaultValue={props.user?.userType}
+          className="rw-input"
+          errorClassName="rw-input rw-input-error"
+          validation={{ required: true }}
+        />
 
         <FieldError name="userType" className="rw-field-error" />
+
+        <Label
+          name="email"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          Email
+        </Label>
 
         <TextField
           name="email"
@@ -61,6 +77,14 @@ const UserForm = (props) => {
         />
 
         <FieldError name="email" className="rw-field-error" />
+
+        <Label
+          name="password"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          Password
+        </Label>
 
         <TextField
           name="password"

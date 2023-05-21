58eb9fb901974c9fad451a1d9f46aa8552dfd334
Júnior Veras <iramarjunhorveras@gmail.com>
2023-05-21 20:21:59 -0300

    TextField components

--- web/src/pages/HistoricPage/HistoricPage.js
+++ web/src/pages/HistoricPage/HistoricPage.js
@@ -1,13 +1,16 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import AppBar from 'src/components/AppBar'
-import Modal from 'src/components/Modal'
-import SpeedDial from 'src/components/SpeedDial'
+import TextField  from 'src/components/TextField'
 
 const HistoricPage = () => {
   return (
     <>
       <AppBar/>
+
+      <TextField
+      />
+
     </>
   )
 }

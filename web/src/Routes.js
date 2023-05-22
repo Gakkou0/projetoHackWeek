fbb49f8529211c3dc4aa18694bd79bf6f889e20b
Gakkou0 <fhugo.625@gmail.com>
2023-05-21 21:28:25 -0300

    refatoração

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -15,6 +15,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/teste" page={TestePage} name="teste"/>
       <Route path="/dashboard" page={DashboardPage} name="dashboard" />
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/" page={HomePage} name="home" />
---
+++ web/src/pages/TestePage/TestePage.stories.js
@@ -0,0 +1,10 @@
+import TestePage from './TestePage'
+
+export const generated = () => {
+  return <TestePage />
+}
+
+export default {
+  title: 'Pages/TestePage',
+  component: TestePage,
+}

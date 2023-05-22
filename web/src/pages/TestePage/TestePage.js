71b654edef7fa2fd3ceb50a6665984798843d200
Júnior Veras <iramarjunhorveras@gmail.com>
2023-05-22 16:53:31 -0300

    Mod AppBar, Modal, SpeedDial, NewUser, DashBoard

--- web/src/pages/TestePage/TestePage.js
+++ web/src/pages/TestePage/TestePage.js
@@ -1,21 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import Card from 'src/components/MeetingCard/MeetingCard'
-
-const TestePage = () => {
-  return (
-    <>
-      <MetaTags title="Teste" description="Teste page" />
-
-      <Card cosupervisor={true} viewer={"student"} id={1}/>
-    </>
-  )
-}
-
-export default TestePage
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
-import Card from 'src/components/MeetingCard/MeetingCard'
 import ModalForm from 'src/components/ModalForm/ModalForm'
 
 const TestePage = () => {
